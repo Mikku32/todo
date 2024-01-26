56aefcec4f32bd177358c56d45e2ff35f3a3e468
Mikdhad.k <mikdhadk32@gmail.com>
2024-01-27 00:14:16 +0530

    .

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -11,113 +11,104 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final HomeController homecontroller = Get.put(HomeController());
     final _todomessagecontroller = TextEditingController();
     return SafeArea(
       child: Scaffold(
-         backgroundColor: Color.fromARGB(96, 214, 210, 210),
-         appBar: AppBar(
-          title: Text('To-Do',style: GoogleFonts.aBeeZee(fontSize: 20),),
+        backgroundColor: Color.fromARGB(96, 214, 210, 210),
+        appBar: AppBar(
+          title: Text(
+            'To-Do',
+            style: GoogleFonts.aBeeZee(fontSize: 20),
+          ),
           centerTitle: true,
           backgroundColor: Color.fromARGB(95, 242, 238, 238),
           elevation: 0,
-          leading: IconButton(onPressed: (){}, icon: Icon(Icons.menu)),
+          leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
           actions: [
             InkWell(
-              onTap: (){},
+              onTap: () {},
               child: CircleAvatar(
-              radius: 15,
-              backgroundImage: AssetImage('assets/xavior.jpg'),),
-            )],
-         ),
+                radius: 15,
+                backgroundImage: AssetImage('assets/xavior.jpg'),
+              ),
+            )
+          ],
+        ),
         body: SingleChildScrollView(
           scrollDirection: Axis.vertical,
-          
           child: Padding(
             padding: const EdgeInsets.all(15.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-             children: [
-              Obx(() =>   Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                              
+              children: [
+                Obx(
+                  () => Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 18.0, vertical: 0),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Search',
+                                prefixIcon: IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(Icons.search),
+                                  color: Color.fromARGB(255, 202, 201, 197),
+                                )),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Text(
+                        "All ToDo's_",
+                        style: GoogleFonts.aBeeZee(
+                            fontSize: 20, fontWeight: FontWeight.normal),
+                      ),
+                      ...homecontroller.todoList
+                          .map((newTodo) =>
+                              ToDoCard(toDoMessage: newTodo.toDoMessage))
+                          .toList(),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(16),
-                    
                   ),
                   child: Padding(
                     padding: const EdgeInsets.symmetric(
-                      horizontal: 18.0,
-                      vertical: 0
-                    ),
+                        horizontal: 18.0, vertical: 0),
                     child: TextField(
+                      controller: _todomessagecontroller,
                       decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Search',
-                        prefixIcon: IconButton(onPressed: (){},
-                         icon: Icon(Icons.search),
-                         color: Color.fromARGB(255, 202, 201, 197),)
-                      ),
+                          border: InputBorder.none,
+                          hintText: 'Add todo',
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              Get.find<HomeController>()
+                                  .addTodo(_todomessagecontroller.text);
+                              _todomessagecontroller.clear();
+                            },
+                            icon: Icon(Icons.add),
+                          )),
                     ),
-                    
-                  ),
-                  
-                  
-                               ),
-                                SizedBox(
-              height: 40,
-             ),
-            Text("All ToDo's_",
-            style: GoogleFonts.aBeeZee(
-              fontSize: 20,
-              fontWeight: FontWeight.normal
-              
-            ),
-            ),
-                                 ...homecontroller.todoList.map((newTodo) => ToDoCard(toDoMessage: newTodo.toDoMessage))
-              .toList(),
-                ],
-              ),
-             
-            ),
-           
-            
-            SizedBox(
-              height: 20,
-            ),
-           
-             Container(
-            
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(16),
-                
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 18.0,
-                  vertical: 0
-                ),
-                child: TextField(
-                  controller: _todomessagecontroller,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Add todo',
-                   
-                    suffixIcon: IconButton(onPressed: (){
-                     Get.find<HomeController>().addTodo(_todomessagecontroller.text);
-                     _todomessagecontroller.clear();
-                     }, icon: Icon(Icons.add),)
                   ),
                 ),
-              ),
-              
-             ),
-             ], 
+              ],
             ),
           ),
         ),
@@ -125,4 +116,3 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
-
